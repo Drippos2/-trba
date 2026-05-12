4aee22ab65f688ddbf5fff54aa0c5533b37ceda0
Používateľ <vortexsten@gmail.com>
2026-05-12 10:25:50 +0200

    upgrade previo2

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = "https://trba.onrender.com/api";
 export const API = `${BACKEND_URL}/api`;
 
 export const api = axios.create({ baseURL: API });
