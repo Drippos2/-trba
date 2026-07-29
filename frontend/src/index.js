dc2e1e0941ea2ff2fed7f59423931b7c3873f4ea
Drippos2 <vortexsten@gmail.com>
2026-07-29 07:50:54 +0200

    fixxos

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // 1. Importuj BrowserRouter
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import { LangProvider } from "./LangContext"; // 2. Importuj svoj LangProvider (uprav cestu ak treba)
+import { LangProvider } from "./context/LangContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* 3. Obal aplikáciu routerom */}
+    <BrowserRouter>
       <LangProvider>
         <App />
       </LangProvider>
