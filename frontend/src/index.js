9a63138553efb9908d0083353c40ffa4ea862134
Drippos2 <vortexsten@gmail.com>
2026-07-29 07:56:38 +0200

    fixxos

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import { LangProvider } from "./contexts/LangContext";
+import { LangProvider } from "./context/LangContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <LangProvider>
-        <App />
-      </LangProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
