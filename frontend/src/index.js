f49fbd35fd2dd118ad0986a4f23a13140bafe0fd
Drippos2 <vortexsten@gmail.com>
2026-07-29 07:53:22 +0200

    fixxos

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import { LangProvider } from "./context/LangContext";
+import { LangProvider } from "./contexts/LangContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
